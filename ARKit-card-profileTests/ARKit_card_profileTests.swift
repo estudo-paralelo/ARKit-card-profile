430b75641028e98c800ab9c2c2fdc7e1f8551fd9
Alexandre Ohara <alexandre1996_@hotmail.com>
2018-09-10 13:08:27 -0300

    Initial Commit

---
+++ ARKit-card-profileTests/ARKit_card_profileTests.swift
@@ -0,0 +1,36 @@
+//
+//  ARKit_card_profileTests.swift
+//  ARKit-card-profileTests
+//
+//  Created by Alexandre Ohara on 10/09/18.
+//  Copyright © 2018 Alexandre Ohara. All rights reserved.
+//
+
+import XCTest
+@testable import ARKit_card_profile
+
+class ARKit_card_profileTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
